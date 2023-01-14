eec232f4967db949e7edb28cacee72e31ee5c4f7
benjamineke <benjiemondia198@gmail.com>
2023-01-14 12:37:46 +0800

    Create index.php

---
+++ 01-14-2023/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="main.css">
+    <title>Login Form</title>
+</head>
+<body>
+    <nav class="navbar navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand">Login System</a>
+            <div class="form-group">
+                <input id="btn" type="button" onclick="SignUp()" name="Sign Up" class="btn btn-dark btn-md" value="Sign Up">
+                <input id="btn" type="button" onclick="SignIn()" name="Sign In" class="btn btn-warning btn-md" value="Sign In">
+            </div>
+        </div>
+    </nav>
+    <div class="container">
+        <center>
+            <h1>Benjie <span>Mondia</span></h1>
+            <p>activity</p>
+        </center>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="main.js"></script>
+</body>
+</html>
